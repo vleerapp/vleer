48a25ff432aed3b1152eb28d2f721fdf03a8d2c6
0PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2025-12-16 01:21:47 +0100

    feat: add reload state hotkey

--- src/ui/global_actions.rs
+++ src/ui/global_actions.rs
@@ -4,11 +4,11 @@ use tracing::{debug, info};
 use crate::{
     data::config::Config,
     media::{playback::Playback, queue::Queue},
+    ui::state::State,
 };
 
-actions!(vleer, [Quit]);
+actions!(vleer, [Quit, ReloadConfig, ReloadState]);
 actions!(player, [PlayPause, Next, Previous]);
-actions!(config, [Reload]);
 
 pub fn register_actions(cx: &mut App) {
     debug!("Registering Actions");
@@ -17,14 +17,16 @@ pub fn register_actions(cx: &mut App) {
     cx.on_action(next);
     cx.on_action(previous);
     cx.on_action(reload_config);
+    cx.on_action(reload_state);
 
     cx.bind_keys([KeyBinding::new("secondary-w", Quit, None)]);
     cx.bind_keys([KeyBinding::new("secondary-q", Quit, None)]);
-    cx.bind_keys([KeyBinding::new("secondary-right", Next, None)]);
-    cx.bind_keys([KeyBinding::new("secondary-left", Previous, None)]);
-    cx.bind_keys([KeyBinding::new("space", PlayPause, None)]);
+    cx.bind_keys([KeyBinding::new("alt-right", Next, None)]);
+    cx.bind_keys([KeyBinding::new("alt-left", Previous, None)]);
+    cx.bind_keys([KeyBinding::new("space", PlayPause, Some("!TextInput"))]);
 
-    cx.bind_keys([KeyBinding::new("secondary-alt-r", Reload, None)]);
+    cx.bind_keys([KeyBinding::new("secondary-alt-r", ReloadConfig, None)]);
+    cx.bind_keys([KeyBinding::new("secondary-r", ReloadState, None)]);
     debug!("Actions: {:?}", cx.all_action_names());
 }
 
@@ -58,8 +60,12 @@ fn next(_: &Next, cx: &mut App) {
     }
 }
 
-fn reload_config(_: &Reload, cx: &mut App) {
+fn reload_config(_: &ReloadConfig, cx: &mut App) {
     if let Err(e) = Config::reload(cx) {
         tracing::error!("Failed to reload config: {}", e);
     }
+}
+
+fn reload_state(_: &ReloadState, cx: &mut App) {
+    State::prepare(cx)
 }
