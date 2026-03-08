2e676b441da5cb5a52b62ca3878df8ff4940da52
PandaDEV <pandadev.net@proton.me>
2026-03-08 17:10:38 +0100

    feat: add a log file to datadir

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@
 )]
 
 use std::sync::LazyLock;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 mod data;
 mod media;
@@ -18,7 +21,28 @@ static RUNTIME: LazyLock<tokio::runtime::Runtime> = LazyLock::new(|| {
 });
 
 fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
+    let data_dir = dirs::data_dir()
+        .expect("couldn't get data directory")
+        .join("vleer");
+
+    std::fs::create_dir_all(&data_dir).ok();
+
+    let log_path = data_dir.join("vleer.log");
+    let log_file = std::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(&log_path)?;
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        .with(tracing_subscriber::fmt::layer())
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_writer(std::sync::Mutex::new(log_file))
+                .with_ansi(false),
+        )
+        .init();
 
     let skip_single_instance = std::env::args().any(|arg| arg == "--skip-single-instance");
 
