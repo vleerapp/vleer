5f66be740ad02d613b5a1bc6f7cfb079b91493ba
PandaDEV <pandadev.net@proton.me>
2026-05-17 16:55:37 +0200

    refactor: simplify icons logic into one file

--- src/ui/components/icons.rs
+++ src/ui/components/icons.rs
@@ -36,3 +36,45 @@ pub const PROPERTIES: &str = "!bundled:icons/properties.svg";
 pub const TRASH: &str = "!bundled:icons/trash.svg";
 pub const PLAY_NEXT: &str = "!bundled:icons/play-next.svg";
 pub const PLAY_LAST: &str = "!bundled:icons/play-last.svg";
+
+use gpui::*;
+
+use crate::ui::variables::Variables;
+
+#[derive(IntoElement)]
+pub struct Icon {
+    svg: Stateful<Svg>,
+}
+
+impl Styled for Icon {
+    fn style(&mut self) -> &mut StyleRefinement {
+        self.svg.style()
+    }
+}
+
+impl InteractiveElement for Icon {
+    fn interactivity(&mut self) -> &mut Interactivity {
+        self.svg.interactivity()
+    }
+}
+
+impl StatefulInteractiveElement for Icon {}
+
+impl RenderOnce for Icon {
+    fn render(mut self, _: &mut gpui::Window, cx: &mut gpui::App) -> impl gpui::IntoElement {
+        let variables = cx.global::<Variables>();
+
+        if self.svg.text_style().color.is_none() {
+            self.svg = self.svg.text_color(variables.text_secondary);
+        }
+
+        self.svg.w(px(16.0)).h(px(16.0)).flex_shrink_0()
+    }
+}
+
+pub fn icon(icon: impl Into<SharedString>) -> Icon {
+    let icon_str: SharedString = icon.into();
+    Icon {
+        svg: svg().path(icon_str.clone()).id(icon_str.clone()),
+    }
+}
